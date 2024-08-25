ac2848a273f98f13a70ce15fc0197261902debc6
SouravNayak10 <146121791+SouravNayak10@users.noreply.github.com>
2024-08-25 11:50:55 +0530

    Add files via upload

---
+++ JavaScript Fundamentals Assignment 1/Q1.js
@@ -0,0 +1,4 @@
+let totalvalue= 2000;
+let discountpercentage= 20;
+let discountvalue=totalvalue-(discountpercentage/100)*totalvalue;
+console.log("the final price after discont is:" +discountvalue);
---
+++ JavaScript Fundamentals Assignment 1/Q3.js
@@ -0,0 +1,3 @@
+letamountInr=850;
+letamountusd=850/82;
+console.log(+ letamountInr + " INR is " +letamountusd + "USD");
